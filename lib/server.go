9f42c33f3483d16cce7229ac0ca97870e2422571
zhangwei <zhangwei@kuaifazs.com>
2018-03-14 19:14:43 +0800

    提交代码

--- lib/server.go
+++ lib/server.go
@@ -57,3 +57,11 @@ func (server *Server) Stop() {
 	server.listener.Close()
 	server.manager.Dispose()
 }
+
+func Serve(network, address string, sendChanSize int) (*Server, error) {
+	listener, err := net.Listen(network, address)
+	if err != nil {
+		return nil, err
+	}
+	return NewServer(listener, sendChanSize), nil
+}
